bb2e814fdf161090b62101a49142756144c6099b
Marthyboo <128302830+Marthyboo@users.noreply.github.com>
2026-01-11 16:29:21 +0000

    Delete src directory

--- src/main/java/com/java/loader/Main.java
+++
@@ -1,9 +0,0 @@
-package com.java.loader;
-
-import com.java.loader.c;
-
-public class Main {
-    public static void main(String[] args) {
-        new c();
-    }
-}
--- src/main/java/com/java/loader/f.java
+++
@@ -1,9 +0,0 @@
-/*
- * Decompiled with CFR 0.152.
- */
-package com.java.loader;
-
-public interface f {
-    public void a(boolean var1);
-}
-
